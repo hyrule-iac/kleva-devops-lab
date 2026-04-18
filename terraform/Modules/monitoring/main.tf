a9687b0ec938e9a22795e3c7ccd4b5c1574f7f30
admin <aldosan666@outlook.com>
2026-04-18 11:40:03 -0600

    Fixed Graphana and prometheus

---
+++ terraform/Modules/monitoring/main.tf
@@ -0,0 +1,30 @@
+# -----------------------------------------------------------------------------------------
+# OBSERVABILITY STACK: Grafana Deployment
+# Strategy: Helm-based deployment with dynamic configuration via Sidecars.
+# -----------------------------------------------------------------------------------------
+# Deploying Grafana through HELM on Kubernetes cluster also into monitoring namespace
+
+resource "helm_release" "grafana" {
+  name       = "grafana"
+  repository = "https://grafana.github.io/helm-charts"
+  chart      = "grafana"
+  namespace  = kubernetes_namespace.monitoring.metadata[0].name
+
+  set {
+    name  = "adminPassword"
+    value = var.grafana_admin_password
+  }
+
+    # DYNAMIC CONFIGURATION: Enabling Sidecar containers to auto-discover 
+    # Dashboards and Datasources stored as Kubernetes ConfigMaps.
+    # This enables a "GitOps" approach for observability dashboards.
+
+  set {
+    name  = "sidecar.dashboards.enabled"
+    value = "true"
+  }
+  set {
+    name  = "sidecar.datasources.enabled"
+    value = "true"
+  }
+}
---
+++ terraform/Modules/monitoring/variables.tf
@@ -0,0 +1,24 @@
+variable "app_namespace" {
+  description = "Namespace para la aplicación .NET"
+  type        = string
+  default     = "app"
+}
+
+variable "monitoring_namespace" {
+  description = "Namespace para las herramientas de monitoreo"
+  type        = string
+  default     = "monitoring"
+}
+
+variable "grafana_admin_password" {
+  description = "Password para el admin de Grafana"
+  type        = string
+  default     = "admin123"
+  sensitive   = true # Esto oculta el valor en los logs
+}
+
+variable "environment" {
+  description = "Ambiente de despliegue"
+  type        = string
+  default     = "dev"
+}
