240bd26a605691b5acb7530c330adb485bc51473
admin <aldosan666@outlook.com>
2026-04-18 14:03:43 -0600

    Fixing Variable expression

--- terraform/Modules/monitoring/main.tf
+++ terraform/Modules/monitoring/main.tf
@@ -8,7 +8,7 @@ resource "helm_release" "grafana" {
   name       = "grafana"
   repository = "https://grafana.github.io/helm-charts"
   chart      = "grafana"
-  namespace  = kubernetes_namespace.monitoring.metadata[0].name
+  namespace  = var.monitoring_namespace
 
   set {
     name  = "adminPassword"
